eacab30d89a242c3accf5e0f2b5a13587a5474a4
nktech-official <76529189+Nktech-Official@users.noreply.github.com>
2023-06-03 16:37:55 +0530

    Added BoardState Toggle and PlayerState

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,45 @@
+import { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const handleSquareClick = position => {
+    if (!squares[position]) {
+      setSquares(currentSquares => {
+        return currentSquares.map((squaresValue, pos) => {
+          if (position === pos) {
+            return isXNext ? 'X' : 'O';
+          }
+          return squaresValue;
+        });
+      });
+      setIsXNext(isXNextCurrent => !isXNextCurrent);
+    }
+  };
+
+  const renderSquare = pos => {
+    return (
+      <Square value={squares[pos]} onClick={() => handleSquareClick(pos)} />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
